cfa70636f02927d0aaa6f32b42d2fc2b66f47eac
Kyriacos Kyriacou <kyriacos.s.kyriacou@gmail.com>
2016-09-21 17:03:58 +0300

    Update

--- example/main.go
+++ example/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	numApplicants := 100
 	numTrials := 10000
+
 	e := secretary.Simulate1e(numApplicants, numTrials)
 	nth := secretary.SimulateNth(numApplicants, numTrials, 4)
 	kth := secretary.SimulateKth(numApplicants, numTrials, int(numApplicants/2)) // successive non-candidate rule generally performs well with k = n / 2
